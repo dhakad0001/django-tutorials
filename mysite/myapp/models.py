d7cf788e0359186f0a98cd97e5ca17b3e0e4a274
dhakad0001 <anandhakad230801@gmail.com>
2023-07-10 23:31:13 +0530

    class-4 code changes

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    roll_no =  models.IntegerField()
+    name = models.CharField(max_length=250)
+    course = models.CharField(max_length=250)
+
+# class Course(models.Model):
+#     pass 
+
+
+# class Departments(models.Model):
+#     pass
